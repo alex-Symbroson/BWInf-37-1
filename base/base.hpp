24c6dd009de8f6f4e48ae00ad150314eb01ac4af
alex-Symbroson <alex.Symbroson@gmail.com>
2018-11-11 12:35:05 +0100

    changed base header

--- base/base.hpp
+++ base/base.hpp
@@ -1,11 +1,17 @@
 
 #include <ctype.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+
+typedef const char* cstr;
 
 #define error(a, ...) \
     fprintf(stderr, "\033[0;33m" a "\033[0;37m\n", ##__VA_ARGS__)
+#define werror(a, ...) \
+    fwprintf(stderr, "\033[0;33m" a "\033[0;37m\n", ##__VA_ARGS__)
 
 using namespace std;
 
